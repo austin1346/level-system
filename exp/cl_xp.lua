73970db80fb367a66ddf73cf05eab4a0770eb489
Austin <austin.greene.1346@gmail.com>
2017-01-21 04:29:44 -0600

    Fixed another bug; yeehaw

--- exp/cl_xp.lua
+++ exp/cl_xp.lua
@@ -2,7 +2,7 @@
 --[ Client ]--
 
 net.Receive( "PlayerLeveledUp", function()
-	local newLevel = net.ReadString()
+	local newLevel = net.ReadUInt()
 
 	surface.PlaySound( "garrysmod/save_load4.wav" )
 	notification.AddLegacy( "Level up!; " .. newLevel, NOTIFY_GENERIC, 5 )
